fa2e9201b151f5be1209465e8a7cd8775bf39b2d
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-02-16 14:54:46 +0100

    premiers exercices

---
+++ decimal.js
@@ -0,0 +1,15 @@
+let nb1 = 0x123
+let nb2 = 0123
+let nb3 = 0b10011001
+let nb4 = 0xdeadbeef
+let nb5 = 0xea7beef
+let nb6 = 0b1111111111111111
+let nb7 = 0777
+
+console.log(nb1.toString(2))
+console.log(nb2.toString(2))
+console.log(nb3.toString(2))
+console.log(nb4.toString(2))
+console.log(nb5.toString(2))
+console.log(nb6.toString(2))
+console.log(nb7.toString(2))
